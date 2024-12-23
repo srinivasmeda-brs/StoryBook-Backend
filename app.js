feac29baa8afbe42ef3966af5390f91266e2b0d5
srinivasmeda-brs <svas77310@gmail.com>
2024-12-23 21:32:02 +0530

    storybook

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'; 
+import dotenv from "dotenv";
 import cors from 'cors';
 
 import connectdb from './src/config/db.js';
-import { notFound, errorHandler } from './src/middleWare/errMiddleWare.js';
+import { notFound, errorHandler } from "./src/middleWare/errMiddleWare.js";
 import userRoute from './src/routes/userRoute.js';
 import storyRoute from './src/routes/storyRoute.js';
 
 const app = express();
 const port = process.env.PORT || 3005;
+
 
 dotenv.config();
 connectdb();
@@ -17,27 +18,19 @@ connectdb();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// CORS configuration
-app.use(cors({
-    origin: 'https://storybook-jbeo.onrender.com', // Make sure this matches your frontend URL
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'], // Include OPTIONS for preflight requests
-    allowedHeaders: ['Content-Type', 'Authorization'], // Headers your frontend may send
-    credentials: true, // Allow cookies or authorization headers
-    preflightContinue: false, // Ensures preflight requests get a response
-    optionsSuccessStatus: 200 // For legacy browsers that might choke on a 204 response to OPTIONS
-}));
+// Allow requests from frontend localhost:5173
+// app.use(cors({
+//     origin: 'https://storybook-igxm.onrender.com/',
+//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+// }));
 
 // Routes
 app.use('/api/users', userRoute);
-app.use('/api', storyRoute);
+app.use('/api/', storyRoute);
 
-// Handle 404 errors (Not Found)
 app.use(notFound);
-
-// Handle errors globally
 app.use(errorHandler);
 
-// Test route
 app.get('/', (req, res) => {
     console.log("hello srinivas");
     res.send("hello srinivas");
